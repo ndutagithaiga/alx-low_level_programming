90a967af3d6c33463ac95a9711f0fcd2767d54bb
Nduta Githaiga <ndutagithaiga11@gmail.com>
2023-02-21 16:50:13 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islowe - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if letteer is lowercase, 0 if not
+ */
+int _is lowercase(int c)
+{
+	if 9c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
